9584f84ac1a6f46323a6029e6db39d30316a0586
Hyperxq <barrientosd9@gmail.com>
2024-02-22 18:04:24 -0600

    feat: version 0.3.12

--- bin/builder.ts
+++ bin/builder.ts
@@ -11,10 +11,15 @@ import { Command, program } from 'commander';
 import { readFileSync } from 'fs';
 import path from 'path';
 import { CommandLoader } from '../commands';
+import { logger } from '../lib/utils';
 import logo from './logo';
+
+const minNodeVersion = 20;
 
 const bootstrap = async () => {
   console.log(logo);
+
+  checkNodeVersion();
 
   program.configureHelp({
     sortSubcommands: true,
@@ -33,6 +38,23 @@ function setVersionFlag(program: Command) {
 
   program.version(version, '-v, --version', 'Output the current version.');
   // program.parse(process.argv);
+}
+
+function checkNodeVersion() {
+  // Extract the major version number
+  const nodeVersion = process.version;
+  const currentMajorVersion = parseInt(
+    nodeVersion.split('.')[0].substring(1),
+    10,
+  );
+
+  // Compare with the minimum required version
+  if (minNodeVersion > currentMajorVersion) {
+    logger.error(
+      `Current Node.js version is ${nodeVersion}. This application requires Node.js v${minNodeVersion} or higher.`,
+    );
+    process.exit(1); // Exit with an error code
+  }
 }
 
 bootstrap();
